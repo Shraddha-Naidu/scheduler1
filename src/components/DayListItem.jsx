3d6b4ea15e9ce6bdc8fba1ca449793d903b9d431
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-28 04:11:36 -0600

    fix for unused variable

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -28,7 +28,7 @@ export default function DayListItem(props) {
       onClick ={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots} spots remaining</h3>
     </li>
   );
 }
