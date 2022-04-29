0f8ae88da77c7a51a556f70d6dd77588f79049af
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-29 13:30:05 -0600

    Add apoointment form stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,15 @@ import React from "react"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
-import Form from "./Form";
-import Status from "./Status";
-import Confirm from "./Confirm";
-import Error from "./Error";
+
 
 import "components/Appointment/styles.scss"
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer}/>: <Empty />}
+    </article>
   )
 }
