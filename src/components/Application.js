560395165edc05b7980a4db837f97a5735adaef9
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-29 14:22:26 -0600

    Remove hard coded Days and use axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,24 +5,6 @@ import "components/Application.scss";
 import DayList from "components/DayList.jsx";
 import Appointment from "components/Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = [
   {
   "1": {
@@ -65,7 +47,14 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+      .then(response => {
+        setDays(response.data);
+      });
+  }, [])
 
   return (
     <main className="layout">
