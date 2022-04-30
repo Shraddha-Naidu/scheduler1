fd11cc047d7b43164474baaf17d58733cfd0b3c4
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-04-29 23:07:46 -0600

    Update transition, back methods for useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,1 +1,21 @@
 import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode) {
+    setHistory((prev) => ([newMode, ...prev]));
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length -1])
+    }
+  }
+
+  return { mode, transition, back };
+};
+
