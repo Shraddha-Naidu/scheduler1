9a717a67f966f936e00b27b508f81cae3e26e0d4
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-10 06:27:09 -0600

    fixed to show number of spots

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-
 import classNames from "classnames";
 
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
-  });
+ });
 
   const formatSpots = function () {
     let remainingSpots;
@@ -22,13 +20,12 @@ export default function DayListItem(props) {
   };
 
   return (
-
     <li
       className={dayClass}
       onClick ={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{formatSpots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()} spots remaining</h3>
     </li>
   );
 }
