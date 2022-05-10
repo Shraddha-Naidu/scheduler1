0b321f196ac716f4bbae4d5976855132f239a3a6
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-05-10 06:27:23 -0600

    Remove extra spacing

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -5,26 +5,26 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-  const interviewers = props.interviewers.map( interviewer => {
+  const allInterviewers = props.interviewers.map((interviewer) => {
     return (
       <InterviewerListItem
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === props.value}
-      setInterviewer={() => props.onChange(interviewer.id)}
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
       />
-    )
+    );
   });
 
   return (
-      <section className="interviewers">
-        <h4 className="interviewers__header text--light">Interviewer</h4>
-        <ul className="interviewers__list">
-          {interviewers}
-        </ul>
-      </section>
-  )
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {allInterviewers}
+      </ul>
+    </section>
+  );
 }
 
 InterviewerList.propTypes = {
